07a11e3553dcd135972d32b9c5fda2906b95ce15
Mufti Alie S <iyakiwan19@gmail.com>
2019-06-18 18:37:48 +0700

    upload sql server 1

--- dbConnect.php
+++ dbConnect.php
@@ -1,10 +1,6 @@
 <?php
- 	$host = "dasprolaboratory.database.windows.net";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
     try {
-        $con = new PDO("sqlsrv:server = dasprolaboratory.database.windows.net; Database = bljrAzure", "mufti", "qwert12345");
+        $con = new PDO("sqlsrv:server = dasprolaboratory.database.windows.net; Database = bljrAzure", "mufti@dasprolaboratory", "qwert12345@");
         $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     } catch(Exception $e) {
         echo "Failed: " . $e;
